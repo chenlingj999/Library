2e2cabc5fe010cac9d7cf28d5c6656bdba4438bc
Ling <126978057+chenlingj999@users.noreply.github.com>
2024-05-28 14:57:49 -0400

    Implement the book prototype

---
+++ object.js
@@ -0,0 +1,16 @@
+function Book(title, author, pageNum, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pageNum = pageNum;
+    this.hasRead = hasRead;
+}
+
+Book.prototype.info = function() {
+    var attribute = [];
+    for (var attr in this) {
+        if (this.hasOwnProperty(attr) && typeof this[attr] !== 'function') {
+            attribute.push(this[attr]);
+        }
+    }
+    return attribute;
+}
