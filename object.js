68bb7f0a76ef1980e9c3467e86fb09f95e248839
Ling <126978057+chenlingj999@users.noreply.github.com>
2024-05-28 20:32:30 -0400

    Add function that adds all books in array to the page

--- object.js
+++ object.js
@@ -21,3 +21,53 @@ function addBookToLibrary(book) {
     if (book instanceof Book) myLibrary.push(book);
 }
 
+//create book cell to add to library
+function createBookCell(book) {
+    if (book instanceof Book) {
+        var cell = document.createElement('div');
+        cell.className = 'book';
+
+        var title = document.createElement('h3');
+        var author = document.createElement('p');
+        var pageNum = document.createElement('p');
+        var hasRead = document.createElement('p');
+
+        title.textContent = 'Title: ' + book.title;
+        author.textContent = 'Author: ' + book.author;
+        pageNum.textContent = 'Page Number: ' + book.pageNum;
+        hasRead.textContent = 'Has Read: ' + (book.hasRead ? 'Yes' : 'No');
+
+        cell.appendChild(title);
+        cell.appendChild(author);
+        cell.appendChild(pageNum);
+        cell.appendChild(hasRead);
+
+        return cell;
+    }
+}
+
+function displayBook() {
+    const library = document.getElementById('library');
+    //Empty library
+    library.innerHTML = '';
+    //Make sure library exists
+    if (!library) console.error('No library found');
+
+    for (var book of myLibrary) {
+        bookCell = createBookCell(book);
+        if (bookCell) {
+            library.appendChild(bookCell);
+        }
+    }
+}
+
+//test
+var b1 = new Book(1, 1, 1, true);
+var b2 = new Book(2, 2, 2, false);
+var b3 = new Book(3, 3, 3, true);
+
+addBookToLibrary(b1);
+addBookToLibrary(b2);
+addBookToLibrary(b3);
+
+displayBook();
